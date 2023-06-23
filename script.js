95d72eaf3645e2c59300ad22486b14624243a7e4
rheeze-dev <ggybzz2297@gmail.com>
2023-06-23 13:11:30 -0400

    Improved logic to display how many words & letters

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ btnSubmit.onclick = () => {
 fetchQuestion(category.value);
 
 async function fetchQuestion(category) {
-    console.log(category);
     const response = await fetch(
         "https://api.api-ninjas.com/v1/trivia?category=" + category,
         {
@@ -25,18 +24,24 @@ async function fetchQuestion(category) {
     }
     const data = await response.json();
     const answerGuide = showLetters(data[0]);
+    // console.log(answerGuide.length);
+    let numberOfWords = 1;
+    for(let i = 0; i < answerGuide.length; i++) {
+        if(answerGuide.charAt(i) == " ") numberOfWords++;
+    }
     document.querySelector("#questions").innerHTML = 
     `<p class="display-questions">${data[0].question}</p>
-    <p class="answer-length">Answer length is: ${data[0].answer.length}</p>
-    <p class="answer-length">${answerGuide.split('').join(' ')}</p>`;
+    <p class="answer-length">Answer is ${numberOfWords} ${numberOfWords == 1 ? "word" : "words"} with ${data[0].answer.length} letters.</p>
+    <p class="answer-length">${answerGuide.split('').join(',')}</p>`;
 }
 
 function showLetters(data) {
     let answer = data.answer;
     const max = data.answer.length;
-    console.log(data.answer);
-    for(let i = 0; i < max / 2; i++) {
-        const randomNumber = Math.round(Math.random() * max);
+    // console.log(data.answer);
+    for(let i = 0; i < max; i++) {
+        const randomNumber = Math.floor(Math.random() * max);
+        if(answer.charAt(randomNumber) == " ") continue;
         answer = setCharAt(answer, randomNumber, '_');
     }
     return answer;
